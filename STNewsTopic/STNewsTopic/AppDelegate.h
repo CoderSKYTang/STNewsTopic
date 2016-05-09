2ea47bb3f6fd7ba9c4aa8aaddf29c7d3f57dbebb
SKYTang <donwei001@qq.com>
2016-05-09 13:44:32 +0800

    初始化项目

---
+++ STNewsTopic/STNewsTopic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STNewsTopic
+//
+//  Created by 研发部 on 16/5/9.
+//  Copyright © 2016年 yanfabu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ STNewsTopic/STNewsTopic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STNewsTopic
+//
+//  Created by 研发部 on 16/5/9.
+//  Copyright © 2016年 yanfabu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
